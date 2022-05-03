75d7d351b4b5f8055c29400c5e90f6f858fc29bd
Error <error>
2022-05-03 15:37:20 +0400

    login presque fini

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/main', function () {
+    return view('layouts/main');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
