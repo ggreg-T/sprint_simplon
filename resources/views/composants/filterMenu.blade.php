23c516cbbe5f0a2659c7b58b4b1ce9fdf7259088
gregory <graigtech@gmail.com>
2022-05-17 11:42:34 +0400

    W3C: fix issue 'The aria-labelledby suite'.

--- resources/views/composants/filterMenu.blade.php
+++ resources/views/composants/filterMenu.blade.php
@@ -6,7 +6,6 @@
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton222">
             <div class="dropdown dropend">
                 <a class="dropdown-item dropdown-toggle" href="#" id="dropdown-layouts-all" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Réunion</a>
-                <!-- <div class="dropdown-menu" aria-label="dropdown-layouts"> -->
                 <div class="dropdown-menu" aria-labelledby="dropdown-layouts-all">
 
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "Reunion", 'filter' => 'all']) }}">aall ++</a>
@@ -19,7 +18,7 @@
 
             <div class="dropdown dropend">
                 <a class="dropdown-item dropdown-toggle" href="#" id="dropdown-layouts-north" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Réunion North</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown-layouts">
+                <div class="dropdown-menu" aria-labelledby="dropdown-layouts-north">
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "north", 'filter' => 'all']) }}">nall ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "north", 'filter' => 'hardDESC']) }}">nHard ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "north", 'filter' => 'hardASC']) }}">nEasy --</a>
@@ -30,7 +29,7 @@
 
             <div class="dropdown dropend">
                 <a class="dropdown-item dropdown-toggle" href="#" id="dropdown-layouts-south" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Réunion South</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown-layouts">
+                <div class="dropdown-menu" aria-labelledby="dropdown-layouts-south">
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "south", 'filter' => 'all']) }}">sall ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "south", 'filter' => 'hardDESC']) }}">sHard ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "south", 'filter' => 'hardASC']) }}">sEasy --</a>
@@ -41,7 +40,7 @@
 
             <div class="dropdown dropend">
                 <a class="dropdown-item dropdown-toggle" href="#" id="dropdown-layouts-west" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Réunion West</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown-layouts">
+                <div class="dropdown-menu" aria-labelledby="dropdown-layouts-west">
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "west", 'filter' => 'all']) }}">wall ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "west", 'filter' => 'hardDESC']) }}">wHard ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "west", 'filter' => 'hardASC']) }}">wEasy --</a>
@@ -52,7 +51,7 @@
 
             <div class="dropdown dropend">
                 <a class="dropdown-item dropdown-toggle" href="#" id="dropdown-layouts-east" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Réunion East</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown-layouts">
+                <div class="dropdown-menu" aria-labelledby="dropdown-layouts-east">
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "east", 'filter' => 'all']) }}">eall ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "east", 'filter' => 'harddesc']) }}">eHard ++</a>
                     <a class="dropdown-item" href="{{ route('getlistTrecks', ['location' => "east", 'filter' => 'hardASC']) }}">eEasy --</a>
