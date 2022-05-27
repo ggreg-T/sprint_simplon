dd4a3c09531fa2c9794a789c26db8ba819a33e16
unknown <stevegreiner68@gmail.com>
2022-05-27 13:29:17 +0400

    delete .

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -22,8 +22,8 @@
     <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.0.9/mapbox-gl-draw.css'
         type='text/css' />
 
-        <link href="./css/userTreck.css" rel="stylesheet" type="text/css">
-        <link href="./css/layout.css" rel="stylesheet" type="text/css">
+        <link href="/css/userTreck.css" rel="stylesheet" type="text/css">
+        <link href="/css/layout.css" rel="stylesheet" type="text/css">
     {{-- ###---for map start & endpoint---########################################################################## --}}
     <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
     <link rel="stylesheet"
@@ -42,7 +42,7 @@
         {{-- script for hover dropdpwn menu treks --}}
         {{-- <script src="{{ url('js/filterMenu.js') }}"></script> --}}
     </main>
-    <script src="./js/filterSorte.js"></script>
+    <script src="/js/filterSorte.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
